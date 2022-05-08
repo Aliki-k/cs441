18733bcddd5d3cc7bc0b617dd0087d97a40d6890
Aliki-k <71377611+Aliki-k@users.noreply.github.com>
2022-05-08 13:46:46 +0300

    Update Client.java

--- epl441project/src/gui/Client.java
+++ epl441project/src/gui/Client.java
@@ -1,13 +1,19 @@
 package gui;
-import java.rmi.registry.LocateRegistry; 
-import java.rmi.registry.Registry; 
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Client {
-	try { 
-        // Getting the registry 
-        Registry registry = LocateRegistry.getRegistry(null); 
-       
-     } catch (Exception e) {
-        System.err.println("Client exception: " + e.toString()); 
-        e.printStackTrace(); 
-     } 
+
+	public static void main(String args[]) {
+
+		try {
+			// Getting the registry
+			Registry registry = LocateRegistry.getRegistry(null);
+
+		} catch (Exception e) {
+			System.err.println("Client exception: " + e.toString());
+			e.printStackTrace();
+		}
+	}
 }
